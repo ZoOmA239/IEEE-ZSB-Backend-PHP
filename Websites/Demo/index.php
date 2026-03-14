ea1429c32d80077267097cd2f4035ee8fe9b600e
HazemHHHH <hazemshradwan@gmail.com>
2026-03-14 15:14:02 +0200

    Separate Logic

--- Websites/Demo/index.php
+++ Websites/Demo/index.php
@@ -1,87 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
+<?Php
+$books = [
+    [
+        'name' => "Harry Potter",
+        'author' => "J.K. Rowling",
+        'year' => 1997,
+        'url' => "https://en.wikipedia.org/wiki/Harry_Potter"
+    ],
+    [
+        'name' => "The Lord of the Rings",
+        'author' => "J.R.R. Tolkien",
+        'year' => 1954,
+        'url' => "https://en.wikipedia.org/wiki/The_Lord_of_the_Rings"
+    ],
+    [
+        'name' => "The Great Gatsby",
+        'author' => "F. Scott Fitzgerald",
+        'year' => 1925,
+        'url' => "https://en.wikipedia.org/wiki/The_Great_Gatsby"
+    ],
+    [
+        'name' => "To Kill a Mockingbird",
+        'author' => "J.K. Rowling",
+        'year' => 1960,
+        'url' => "https://en.wikipedia.org/wiki/To_Kill_a_Mockingbird"
+    ]
 
-<head>
-    <title>Demo</title>
-    <!-- <style>
-        body {
-            display: grid;
-            place-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: sans-serif;
+];
+
+
+
+function filter($items, $function)
+{
+    $filtereditems = [];
+    foreach ($items as $item) {
+        if ($function($item)) {
+            $filtereditems[] = $item;
         }
-    </style> -->
-</head>
-
-<body>
-
-    <h1>Recommeded Books </h1>
-
-    <?Php
-    $books = [
-        [
-            'name' => "Harry Potter",
-            'author' => "J.K. Rowling",
-            'year' => 1997,
-            'url' => "https://en.wikipedia.org/wiki/Harry_Potter"
-        ],
-        [
-            'name' => "The Lord of the Rings",
-            'author' => "J.R.R. Tolkien",
-            'year' => 1954,
-            'url' => "https://en.wikipedia.org/wiki/The_Lord_of_the_Rings"
-        ],
-        [
-            'name' => "The Great Gatsby",
-            'author' => "F. Scott Fitzgerald",
-            'year' => 1925,
-            'url' => "https://en.wikipedia.org/wiki/The_Great_Gatsby"
-        ],
-        [
-            'name' => "To Kill a Mockingbird",
-            'author' => "J.K. Rowling",
-            'year' => 1960,
-            'url' => "https://en.wikipedia.org/wiki/To_Kill_a_Mockingbird"
-        ]
-
-    ];
-
-
-
-    function filter($items, $function)
-    {
-        $filtereditems = [];
-        foreach ($items as $item) {
-            if ($function($item)) {
-                $filtereditems[] = $item;
-            }
-        }
-        return $filtereditems;
     }
+    return $filtereditems;
+}
 
 
 
 
-    $filteredBooks = filter($books, function ($book) {
-        return $book['year'] < "1960";
-    });
-
-    ?>
+$filteredBooks = filter($books, function ($book) {
+    return $book['year'] < "1960";
+});
 
 
-
-
-
-    <ul>
-        <?php foreach ($filteredBooks as $book) : ?>
-            <li>
-                <a href="<?php echo $book['url']; ?>">
-                    <?php echo $book['name']; ?> (<?php echo $book['year']; ?>) by <?php echo $book['author']; ?>
-                </a>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-</body>
-
-</html>
+require "index.view.php";
