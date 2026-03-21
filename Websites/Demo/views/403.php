d1e8b26202a1baaaca221d39177e8558c16bff92
HazemHHHH <hazemshradwan@gmail.com>
2026-03-21 15:54:38 +0200

    Authorization

---
+++ Websites/Demo/views/403.php
@@ -0,0 +1,15 @@
+<?php require "views/partials/head.php"; ?>
+<?php require "views/partials/nav.php"; ?>
+
+<main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 class="text-2xl font-bold">You are unauthorized to access this page
+
+        </h1>
+        <p class="mt-4">
+            <a href="/" class="text-blue underline">Go back to the homepage</a>
+        </p>
+    </div>
+</main>
+
+<?php require "views/partials/footer.php"; ?>
