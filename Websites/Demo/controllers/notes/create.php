21335273694174b29bb14123b680140760651927
HazemHHHH <hazemshradwan@gmail.com>
2026-03-26 17:30:15 +0200

    Autoloading and Extractions

--- Websites/Demo/controllers/notes/create.php
+++ Websites/Demo/controllers/notes/create.php
@@ -1,17 +1,16 @@
 <?php
 
-require "Validator.php";
+require base_path("Validator.php");
 
-$config = require "config.php";
+$config = require  base_path("config.php");
 $db = new Database($config['Database']);
 
 
-$heading = "Create Note";
 
+$errors = [];
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    $errors = [];
     if (! Validator::string($_POST['body'], 1, 1000)) {
         $errors['body'] = "A body of no more than 1000 characters is required";
     }
@@ -23,4 +22,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         ]);
     }
 }
-require "views/notes/create.view.php";
+
+view("notes/create.view.php", [
+    'heading' => "Create Note",
+    'errors' => $errors
+]);
