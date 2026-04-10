f376d5f6368f441c3974d54af79142cfa93e2314
HazemHHHH <hazemshradwan@gmail.com>
2026-04-10 17:37:18 +0200

    The PRG Pattern

--- Websites/Demo/Core/Authenticator.php
+++ Websites/Demo/Core/Authenticator.php
@@ -41,10 +41,6 @@ class Authenticator
 
     public function logout()
     {
-        $_SESSION = [];
-        session_destroy();
-
-        $params = session_get_cookie_params();
-        setcookie('PHPSESSID', '', time() - 3600, $params['domain'], $params['secure'], $params['httponly']);
+        Session::destroy();
     }
 }
